5763fe0595f705c215e8804fc19e7969554916bb
JulienDemarque <julien_demarque@hotmail.fr>
2017-08-13 18:35:34 +0300

    add navbar and navigation

---
+++ server.js
@@ -0,0 +1,21 @@
+
+// Require the stuff we need
+var express = require("express");
+var http = require("http");
+// Build the app
+var app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.render('home.ejs');
+});
+
+app.get("/chordapp", function(req, res) {
+  res.render('index.ejs');
+});
+
+http.createServer(app).listen(8080, function(){
+	console.log("server running");
+});
+
