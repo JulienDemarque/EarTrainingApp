7baeebff5837dfd014e174c1625b0620b71ba858
JulienDemarque <julien_demarque@hotmail.fr>
2017-09-24 01:01:25 +0300

    fed the data base and more

--- public/app.js
+++ public/app.js
@@ -5,63 +5,21 @@ $(document).ready(function(){
 // add info button
 
 
-    var data = [
-        {
-        title: "Coquette (medium)",  
-        key: "D",
-        sheet: [
-            {divided: false, chord:"D", exten:"maj69"},
-            {divided: false, chord:"D", exten:"maj69"},
-            {divided: false, chord:"E", exten:"min7"},
-            {divided: false, chord:"A", exten:"7"},
-            {divided: false, chord:"E", exten:"min7"}, 
-            {divided: false, chord:"A", exten:"7"}, 
-            {divided: false, chord:"D", exten:"maj69"}, 
-            {divided: true, chord:"E", exten:"min7"},
-            {divided: true, chord:"A", exten:"7"},
-            {divided: false, chord:"D", exten:"maj69"},
-            {divided: false, chord:"D", exten:"maj69"},
-            {divided: false, chord:"E", exten:"min7"},
-            {divided: false, chord:"A", exten:"7"},
-            {divided: false, chord:"E", exten:"min7"}, 
-            {divided: false, chord:"A", exten:"7"}, 
-            {divided: false, chord:"D", exten:"maj69"}, 
-            {divided: false, chord:"D", exten:"maj69"},
-            {divided: false, chord:"D", exten:"7"},
-            {divided: false, chord:"D", exten:"7"},
-            {divided: false, chord:"G", exten:"maj6"},
-            {divided: false, chord:"G", exten:"maj6"},
-            {divided: false, chord:"E", exten:"7"},
-            {divided: false, chord:"E", exten:"7"},
-            {divided: false, chord:"E", exten:"min7"},
-            {divided: false, chord:"A", exten:"7"}, 
-            {divided: false, chord:"D", exten:"maj69"},
-            {divided: false, chord:"D", exten:"maj69"},
-            {divided: false, chord:"E", exten:"min7"},
-            {divided: false, chord:"A", exten:"7"},
-            {divided: false, chord:"E", exten:"min7"}, 
-            {divided: false, chord:"A", exten:"7"}, 
-            {divided: false, chord:"D", exten:"maj69"}, 
-            {divided: true, chord:"E", exten:"min7"},
-            {divided: true, chord:"A", exten:"7"},
-
-
-          ],
-        audio: "url"
-      }
-    ];
-
 
     function randomSong(){
       return Math.floor(Math.random()*data.length);
     }
 
     
-
     var song= data[randomSong()];
 
-    //Logic here for react or js: make a loop through array. if divided is false, add a simple cell, if divided is true, add a divided cell and add i++ (so you don't create 2 divided cells).
-    //test with container div:
+    function setAudio(){
+    	var audio = document.getElementById('audio');
+    	$("source").attr("src", "./audio/" + song.audio +".mp3");
+    	audio.load()
+    }
+
+    setAudio();
 
     function buildSheet(){
         $("#key").text(song.key);
@@ -87,7 +45,9 @@ $(document).ready(function(){
 
 //handle button change song
     function changeSong(){
+
       song= data[randomSong()];
+      setAudio();	
       $(".lead-sheet").empty();
       resetData();
       buildSheet();
@@ -95,6 +55,7 @@ $(document).ready(function(){
     }
 
     function resetData(){
+    $("#song-title").text("");	
       answer= [];
       buildAnswerArray();
     }
@@ -178,10 +139,18 @@ addEventListeners();
         displayExt = "<sup>6</sup>";
       } else if(extension == "maj69"){
         displayExt = "<span class='supsub'><sup>6</sup><sub>9</sub></span>";
+      } else if(extension == "min"){
+        displayExt = "<sub>m</sub>";
       } else if(extension == "min7"){
         displayExt = "<sub>m</sub><sup>7</sup>";
+      } else if(extension == "min6"){
+        displayExt = "<sub>m</sub><sup>6</sup>";
       } else if(extension == "7"){
         displayExt = "<sup>7</sup>";
+      } else if(extension == "9"){
+        displayExt = "<sup>9</sup>";
+      } else if(extension == "dim7"){
+        displayExt = "<sup>O</sup>";
       }
       fullChord = chord + displayExt;
       $(cell).html(fullChord);
@@ -231,6 +200,7 @@ addEventListeners();
     });
 
     function showAnswer(){
+    	$("#song-title").text("Song Title: " + song.title);
       for (i=0; i<song.sheet.length; i++){
         chord= song.sheet[i].chord;
         extension= song.sheet[i].exten;
@@ -241,4 +211,195 @@ addEventListeners();
 
 
 });
+    var data = [
+        {
+        title: "Coquette",  
+        key: "D",
+        sheet: [
+            {divided: false, chord:"D", exten:"maj69"},
+            {divided: false, chord:"D", exten:"maj69"},
+            {divided: false, chord:"E", exten:"min7"},
+            {divided: false, chord:"A", exten:"7"},
+            {divided: false, chord:"E", exten:"min7"}, 
+            {divided: false, chord:"A", exten:"7"}, 
+            {divided: false, chord:"D", exten:"maj69"}, 
+            {divided: true, chord:"E", exten:"min7"},
+            {divided: true, chord:"A", exten:"7"},
+            {divided: false, chord:"D", exten:"maj69"},
+            {divided: false, chord:"D", exten:"maj69"},
+            {divided: false, chord:"E", exten:"min7"},
+            {divided: false, chord:"A", exten:"7"},
+            {divided: false, chord:"E", exten:"min7"}, 
+            {divided: false, chord:"A", exten:"7"}, 
+            {divided: false, chord:"D", exten:"maj69"}, 
+            {divided: false, chord:"D", exten:"maj69"},
+            {divided: false, chord:"D", exten:"7"},
+            {divided: false, chord:"D", exten:"7"},
+            {divided: false, chord:"G", exten:"maj"},
+            {divided: false, chord:"G", exten:"maj"},
+            {divided: false, chord:"E", exten:"7"},
+            {divided: false, chord:"E", exten:"7"},
+            {divided: false, chord:"E", exten:"min7"},
+            {divided: false, chord:"A", exten:"7"}, 
+            {divided: false, chord:"D", exten:"maj69"},
+            {divided: false, chord:"D", exten:"maj69"},
+            {divided: false, chord:"E", exten:"min7"},
+            {divided: false, chord:"A", exten:"7"},
+            {divided: false, chord:"E", exten:"min7"}, 
+            {divided: false, chord:"A", exten:"7"}, 
+            {divided: false, chord:"D", exten:"maj69"}, 
+            {divided: true, chord:"E", exten:"min7"},
+            {divided: true, chord:"A", exten:"7"}
+          ],
+        audio: "Coquette Ear App"
+      },
+      {
+        title: "Blues 1",  
+        key: "G",
+        sheet: [
+            {divided: false, chord:"G", exten:"maj"},
+            {divided: false, chord:"C", exten:"7"},
+            {divided: false, chord:"G", exten:"maj"},
+            {divided: false, chord:"G", exten:"7"},
+            {divided: false, chord:"C", exten:"7"}, 
+            {divided: false, chord:"C", exten:"7"}, 
+            {divided: false, chord:"G", exten:"maj"}, 
+            {divided: false, chord:"G", exten:"maj"},
+            {divided: false, chord:"D", exten:"7"},
+            {divided: false, chord:"C", exten:"7"},
+            {divided: false, chord:"G", exten:"maj"},
+            {divided: false, chord:"D", exten:"7"}
+          ],
+        audio: "Blues 1 Ear App"
+      },
+      {
+        title: "Blues 2",  
+        key: "G",
+        sheet: [
+            {divided: false, chord:"G", exten:"maj"},
+            {divided: false, chord:"C", exten:"7"},
+            {divided: false, chord:"G", exten:"maj"},
+            {divided: false, chord:"G", exten:"7"},
+            {divided: false, chord:"C", exten:"7"}, 
+            {divided: false, chord:"C", exten:"7"}, 
+            {divided: false, chord:"G", exten:"maj"}, 
+            {divided: false, chord:"E", exten:"7"},
+            {divided: false, chord:"A", exten:"min7"},
+            {divided: false, chord:"D", exten:"7"},
+            {divided: true, chord:"G", exten:"maj"},
+            {divided: true, chord:"E", exten:"7"},
+            {divided: true, chord:"A", exten:"7"},
+            {divided: true, chord:"D", exten:"7"}
+          ],
+        audio: "Blues 2 Ear App"
+      },
+      {
+        title: "Blues 3",  
+        key: "G",
+        sheet: [
+            {divided: false, chord:"G", exten:"maj"},
+            {divided: false, chord:"C", exten:"7"},
+            {divided: false, chord:"G", exten:"maj"},
+            {divided: true, chord:"D", exten:"min7"},
+            {divided: true, chord:"G", exten:"7"},
+            {divided: false, chord:"C", exten:"7"}, 
+            {divided: false, chord:"C#/Db", exten:"dim7"}, 
+            {divided: false, chord:"G", exten:"maj"}, 
+            {divided: false, chord:"E", exten:"7"},
+            {divided: false, chord:"A", exten:"min7"},
+            {divided: false, chord:"D", exten:"7"},
+            {divided: true, chord:"G", exten:"maj"},
+            {divided: true, chord:"E", exten:"7"},
+            {divided: true, chord:"A", exten:"7"},
+            {divided: true, chord:"D", exten:"7"}
+          ],
+        audio: "Blues 3 Ear App"
+      },
+      {
+        title: "Minor Swing",  
+        key: "A",
+        sheet: [
+            {divided: false, chord:"A", exten:"min6"},
+            {divided: false, chord:"A", exten:"min6"},
+            {divided: false, chord:"D", exten:"min"},
+            {divided: false, chord:"D", exten:"min"},
+            {divided: false, chord:"E", exten:"7"}, 
+            {divided: false, chord:"E", exten:"7"}, 
+            {divided: false, chord:"A", exten:"min6"}, 
+            {divided: false, chord:"A", exten:"min6"},
+            {divided: false, chord:"D", exten:"min"},
+            {divided: false, chord:"D", exten:"min"},
+            {divided: false, chord:"A", exten:"min6"},
+            {divided: false, chord:"A", exten:"min6"},
+            {divided: false, chord:"Bb", exten:"maj6"},
+            {divided: false, chord:"E", exten:"7"},
+            {divided: false, chord:"A", exten:"min6"},
+            {divided: false, chord:"E", exten:"7"}
+          ],
+        audio: "Minor Swing Ear App"
+      },
+      {
+        title: "I can't give you anything but love",  
+        key: "G",
+        sheet: [
+            {divided: false, chord:"G", exten:"maj6"},
+            {divided: true, chord:"G", exten:"maj"},
+            {divided: true, chord:"A#/Bb", exten:"dim7"},
+            {divided: false, chord:"A", exten:"min7"},
+            {divided: false, chord:"D", exten:"9"}, 
+            {divided: false, chord:"G", exten:"maj6"},
+            {divided: true, chord:"G", exten:"maj"},
+            {divided: true, chord:"A#/Bb", exten:"dim7"},
+            {divided: false, chord:"A", exten:"min7"},
+            {divided: false, chord:"D", exten:"9"}, 
+            {divided: false, chord:"D", exten:"min7"}, 
+            {divided: false, chord:"G", exten:"7"}, 
+            {divided: false, chord:"C", exten:"maj69"}, 
+            {divided: false, chord:"C", exten:"maj69"}, 
+            {divided: false, chord:"A", exten:"7"}, 
+            {divided: false, chord:"A", exten:"7"}, 
+            {divided: false, chord:"D", exten:"9"}, 
+            {divided: false, chord:"D", exten:"9"}, 
+            {divided: false, chord:"G", exten:"maj6"},
+            {divided: true, chord:"G", exten:"maj"},
+            {divided: true, chord:"A#/Bb", exten:"dim7"},
+            {divided: false, chord:"A", exten:"min7"},
+            {divided: false, chord:"D", exten:"9"}, 
+            {divided: false, chord:"D", exten:"min7"}, 
+            {divided: false, chord:"G", exten:"7"}, 
+            {divided: false, chord:"C", exten:"maj69"}, 
+            {divided: false, chord:"C", exten:"maj69"}, 
+            {divided: false, chord:"C", exten:"maj69"}, 
+            {divided: false, chord:"C#/Db", exten:"dim7"}, 
+            {divided: false, chord:"G", exten:"maj6"}, 
+            {divided: false, chord:"E", exten:"7"}, 
+            {divided: false, chord:"A", exten:"min7"}, 
+            {divided: false, chord:"D", exten:"9"}, 
+            {divided: false, chord:"G", exten:"maj6"}, 
+            {divided: false, chord:"D", exten:"9"}
+          ],
+        audio: "I can't give you anything but love Ear App"
+      },
+      {
+        title: "St James Infirmary Blues",  
+        key: "D",
+        sheet: [
+            {divided: true, chord:"D", exten:"min"},
+            {divided: true, chord:"A", exten:"7"},
+            {divided: false, chord:"D", exten:"min"},
+            {divided: true, chord:"D", exten:"min"},
+            {divided: true, chord:"G", exten:"min"}, 
+            {divided: false, chord:"A", exten:"7"}, 
+            {divided: true, chord:"D", exten:"min"}, 
+            {divided: true, chord:"A", exten:"7"},
+            {divided: false, chord:"D", exten:"min"},
+            {divided: true, chord:"Bb", exten:"7"},
+            {divided: true, chord:"A", exten:"7"},
+            {divided: true, chord:"D", exten:"min"},
+            {divided: true, chord:"A", exten:"7"}
+          ],
+        audio: "St James Infirmary Ear App"
+      }
+    ];
+
 
