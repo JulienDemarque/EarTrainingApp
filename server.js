b15e49e4222d67cf40dbf711b473ad1683f56aaa
julien demarque <julien_demarque@hotmail.fr>
2018-06-29 23:02:43 +0300

    fixed bootstrap, styled forms, use environement variables to hide password

--- server.js
+++ server.js
@@ -2,11 +2,10 @@
 /*jslint es6 */
 "use strict";
 
-
 //database object serves as a temporary container for updating the database
-const database = {level : [
-  {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}
-]};
+const database = {
+  level: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+};
 
 var express = require("express");
 var nodemailer = require("nodemailer");
@@ -23,8 +22,7 @@ var User = require("./models/user");
 // Note: test is the default db when we open the mongo shell, I could have created another one
 //mongoose.connect("mongodb://localhost/test");
 //I should have give an other name than "mydatabase"...
-//mongoose.connect("mongodb://juliendemarque:voodoo1@ds121251.mlab.com:21251/mydatabase");
-
+//the URI of the mlab in store in process.env.DATABASEURL in heroku (see settings)
 
 //we use environnement variables eventually
 mongoose.connect(process.env.DATABASEURL);
@@ -37,13 +35,16 @@ var Score = mongoose.model("Score", scoreSchema);
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 //PASSPORT CONFIGURATION
+
 app.use(require("express-session")({
-  //we might want to use environnement parameter for the secret value later. hash code is hard to reverse but still
-  secret: "my-secret-hash-code",
+  //We created a environnent variable with same value locally and on heroku for safety.
+  secret: process.env.SECRETHASHCODE,
   resave: false,
   saveUninitialized: false
 }));
@@ -53,7 +54,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 //we are making currentUser variable holding the user data available everywhere on client-side
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
   res.locals.currentUser = req.user;
   next();
 });
@@ -76,7 +77,7 @@ app.get("/chordnote", function(req, res) {
 
 //we have the middleware isLoggedIn that will shortcut the callback if we are not login
 //another way would be to not send the request at all from client-side
-app.post("/chordnote", isLoggedIn, function(req, res){
+app.post("/chordnote", isLoggedIn, function(req, res) {
   //console.log("line 100 : ", req.user);
   //console.log(req.body.results);
   let dataFromClient = req.body.results;
@@ -84,31 +85,40 @@ app.post("/chordnote", isLoggedIn, function(req, res){
   let year = today.getFullYear();
   let month = today.getMonth() + 1;
   let date = today.getDate();
-  let fullDate = "" + date + "-"+ month + "-"+ year;
+  let fullDate = "" + date + "-" + month + "-" + year;
   //We are about to connect to database and retrive the user results...
-  Score.find({"name": req.user.username}, function(err, score){
-    if(err){
+  Score.find({
+    "name": req.user.username
+  }, function(err, score) {
+    if (err) {
       console.log("something went wrong in the update");
     } else {
       console.log(score);
-        console.log("Find on mongodb line 88 : ", score[0].results);
-        database.level = score[0].results.level;
-        updateDatabase(req.user.username);
+      //we get an array of found documents, even if there is only one (hopefully!)
+      console.log("Find on mongodb line 97 : ", score[0].results.level);
+      database.level = score[0].results.level;
+      updateDatabase(req.user.username);
 
     }
   });
 
-  function updateDatabase(username){
-    dataFromClient.forEach(function(result){
+  function updateDatabase(username) {
+    dataFromClient.forEach(function(result) {
       //If we don't have result for this date we create the object for today
-      if(database.level[result.level][fullDate] === undefined){
-        if(result.success){
-          database.level[result.level][fullDate] = {rightAnswers : 1, wrongAnswers: 0};
+      if (database.level[result.level][fullDate] === undefined) {
+        if (result.success) {
+          database.level[result.level][fullDate] = {
+            rightAnswers: 1,
+            wrongAnswers: 0
+          };
         } else {
-          database.level[result.level][fullDate] = {rightAnswers : 0, wrongAnswers: 1};
+          database.level[result.level][fullDate] = {
+            rightAnswers: 0,
+            wrongAnswers: 1
+          };
         }
       } else {
-        if(result.success){
+        if (result.success) {
           database.level[result.level][fullDate].rightAnswers += 1;
         } else {
           database.level[result.level][fullDate].wrongAnswers += 1;
@@ -117,8 +127,16 @@ app.post("/chordnote", isLoggedIn, function(req, res){
     });
 
     //we modified our database object, now we put it in the mongo db
-    Score.findOneAndUpdate({"name": username}, {$set:{"results": database}}, {new: true}, function(err, user){
-      if(err){
+    Score.findOneAndUpdate({
+      "name": username
+    }, {
+      $set: {
+        "results": database
+      }
+    }, {
+      new: true
+    }, function(err, user) {
+      if (err) {
         console.log("something went wrong in the update");
       } else {
         console.log(user);
@@ -133,72 +151,94 @@ app.post("/chordnote", isLoggedIn, function(req, res){
 //------------------- AUTH ROUTES------------------
 
 // show register form
-app.get("/register", function(req, res){
+app.get("/register", function(req, res) {
   res.render("register.ejs");
 });
 
 //handle sign up logic
-app.post("/register", function(req, res){
-  var newUser = new User({username: req.body.username});
-  User.register(newUser, req.body.password, function(err, user){
-    if(err){
+app.post("/register", function(req, res) {
+  var newUser = new User({
+    username: req.body.username
+  });
+  User.register(newUser, req.body.password, function(err, user) {
+    if (err) {
       console.log(err);
       return res.render("register.ejs");
     }
     //we want to create a empty score that we will update.
     createEmptyScore(req.body.username);
-    passport.authenticate("local")(req, res, function(){
+    passport.authenticate("local")(req, res, function() {
       res.redirect("chordnote");
     });
   })
 });
 
-function createEmptyScore(username){
+function createEmptyScore(username) {
   //Ok this is some weirdness, mongo has converted my empty object literals to null, so I filled them with one bullshit property
-  const newResults = {level : [
-    {level: 0}, {level: 1}, {level: 2}, {level: 3}, {level: 4}, {level: 5}, {level: 6}, {level: 7}, {level: 8}, {level: 9}, {level: 10}
-  ]};
+  const newResults = {
+    level: [{
+      level: 0
+    }, {
+      level: 1
+    }, {
+      level: 2
+    }, {
+      level: 3
+    }, {
+      level: 4
+    }, {
+      level: 5
+    }, {
+      level: 6
+    }, {
+      level: 7
+    }, {
+      level: 8
+    }, {
+      level: 9
+    }, {
+      level: 10
+    }]
+  };
   var emptyScore = new Score({
     name: username,
     results: newResults
   });
-  emptyScore.save(function(err, score){
-      if(err){
-        console.log("something went wrong");
-      } else {
-        console.log("we saved a new Score to database");
-        console.log(score);
-      }
+  emptyScore.save(function(err, score) {
+    if (err) {
+      console.log("something went wrong");
+    } else {
+      console.log("we saved a new Score to database");
+      console.log(score);
+    }
   });
 }
 
 //show login form
-app.get("/login", function(req, res){
+app.get("/login", function(req, res) {
   res.render("login.ejs");
 })
 
 //handling login logic
-app.post("/login", passport.authenticate("local",
-  {
-    successRedirect: "/chordnote",
-    failureRedirect: "/login"
-  }), function(req, res){
-});
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/chordnote",
+  failureRedirect: "/login"
+}), function(req, res) {});
 
 // logout route
-app.get("/logout", function(req, res){
+app.get("/logout", function(req, res) {
   req.logout();
   res.redirect("/");
 });
 
 //------------- MIDDLEWARE --------------
 // this is a custom middleware. We don't want to try to get to the database if the user is not login
-  function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-      return next();
-    }
-    res.send("need to login for saving data");
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
   }
+  res.send("need to login for saving data");
+}
 
 //---------------------------CONTACT ROUTES ----------
 
@@ -211,44 +251,45 @@ app.get("/confirm", function(req, res) {
 });
 
 app.post("/contact", function(req, res) {
-	var email = req.body.email;
-	var name = req.body.name;
-	var comment = req.body.comment;
+  var email = req.body.email;
+  var name = req.body.name;
+  var comment = req.body.comment;
 
-	var smtpTransport = nodemailer.createTransport({
-       service: "Gmail",
-       secure: true,
-       auth: {
-       user: "juliendemarquedev@gmail.com",
-       pass: "demar0241"
-       }
-   	});
+  var smtpTransport = nodemailer.createTransport({
+    service: "Gmail",
+    secure: true,
+    auth: {
+      user: "juliendemarquedev@gmail.com",
+      //I use an other environnement variable for the password here
+      pass: process.env.EMAILPASSWORD
+    }
+  });
 
 
 
-	var helperOptions= {
-		//email options
-	   from: name + "<" + email + ">",
-	   to: "Julien Dev <juliendemarquedev@gmail.com>", // receiver
-	   subject: "Emailing with nodemailer", // subject
-	   html: "Message from : " + email + "<br/><br/>" + comment // body
-	};
+  var helperOptions = {
+    //email options
+    from: name + "<" + email + ">",
+    to: "Julien Dev <juliendemarquedev@gmail.com>", // receiver
+    subject: "Emailing with nodemailer", // subject
+    html: "Message from : " + email + "<br/><br/>" + comment // body
+  };
 
-	 smtpTransport.sendMail(helperOptions, function(error, info){  //callback
-	        if(error){
-	           return console.log(error);
-	        }else{
-	           console.log("Message sent: " + res.message);
-	       }
+  smtpTransport.sendMail(helperOptions, function(error, info) { //callback
+    if (error) {
+      return console.log(error);
+    } else {
+      console.log("Message sent: " + res.message);
+    }
 
-	  smtpTransport.close();
-	    });
+    smtpTransport.close();
+  });
 
-    res.redirect("/confirm");
+  res.redirect("/confirm");
 });
 
 
 
-app.listen(process.env.PORT || 8080, process.env.IP, function(){
-    console.log("server started");
+app.listen(process.env.PORT || 8080, process.env.IP, function() {
+  console.log("server started");
 });
